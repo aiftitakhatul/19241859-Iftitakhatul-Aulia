e0a5a2514ccd3c1a4e03a8f97a4482c3a19d1f7e
aiftitakhatul <aiftitakhatul@gmail.com>
2025-05-08 09:43:31 +0700

    initialize commit

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+    <h1>BANGUN DATAR</h1>
+    <ul>
+        <li><a href="belah_ketupat copy.php">Belah ketupat</a></li>
+        <li><a href="jajargenjang.php">Jajar Genjang</a></li>
+        <li><a href="persegi_panjang.php">Persegi panjang</a></li>
+        <li><a href="persegi.php">Persegi</a></li>
+        <li><a href="lingkaran.php">Lingkaran</a></li>
+        <li><a href="trapesium.php">Trapesium</a></li>
+        <li><a href="layang_layang.php">Layang-layang</a></li>
+        <li><a href="segitiga.php">Segitiga</a></li>
+    </ul>
+</body>
+</html>
---
+++ jajargenjang.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Jajar genjang</title>
+</head>
+<body>
+    <a href="index.php">Home</a>
+    <h1> Jajar genjang</h1>
+</body>
+</html>
