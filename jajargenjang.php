ec8300535df677535ebca4a65e8682122fb9a596
aiftitakhatul <aiftitakhatul@gmail.com>
2025-05-09 11:20:48 +0700

    ok

--- jajargenjang.php
+++ jajargenjang.php
@@ -6,10 +6,19 @@
     <title>Jajar genjang</title>
 </head>
 <body>
+<?php
+    $alas=16;
+    $tinggi=8;
+    $sisia=10;
+    $sisib=8;
+    $luas=$alas*$tinggi;
+    $keliling=2*($sisia + $sisib);
+    ?>
     <a href="index.php">Home</a>
-    <h1> Jajar genjang</h1>
-    saya 
-
+    <h1>Jajar Genjang</h1>
+    <h4>luas = alas * tinggi = <?php echo $luas;?></h4>
+    <h4>keliling = 2 * (sisia + sisib) = <?php echo $keliling;?></h4>
 
 </body>
+<img src="123663_720.jpg" width="" height="300" alt="">
 </html>
